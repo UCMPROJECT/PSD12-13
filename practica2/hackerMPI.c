3796077c54b708aabf6028c379837d5ac0c5bc31
ocrespo <eoni48@gmail.com>
2013-01-09 19:51:51 +0100

    Practica 2 medio hecho

--- practica2/hackerMPI.c
+++ practica2/hackerMPI.c
@@ -8,19 +8,50 @@
 
 int main(int argc, char* argv[]) {
 
-  int size, rank;
- 
+	int size, rank;
+	int data[3];
+	int ok[3] = {1,1,1};
+	int res[3][3] = {{0,0,0},
+					{0,0,0},
+					{0,0,0}};
 
-  	  // Init
-  	  MPI_Init(&argc, &argv);
-  	  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  	  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-  	  // Some code here!!!
+	// Init
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
 
-  MPI_Finalize();
-  exit(0);
+
+	MPI_Scatter(res,9,MPI_INT,data,3,MPI_INT,0,MPI_COMM_WORLD);
+
+	int end = 0,aux_ok = 1;
+	int i;
+	while(end == 0){
+		compareMatrixRow(data,ok,rank);
+		for(i = 0;i<=3;i++){
+			if(ok[i] == 1){
+				data[i]++;
+				aux_ok = 0;
+			}
+		}
+		if(aux_ok == 1){
+			end = 1;
+		}
+	}
+	MPI_Gather(res,9,MPI_INT,data,3,MPI_INT,0,MPI_COMM_WORLD);
+
+	int finish_him = checkCurrentKey(res);
+
+	if(finish_him == 1){
+		printf("Conseguido");
+	}
+	else{
+		printf("FAIL");
+	}
+
+
+	MPI_Finalize();
+	exit(0);
 }
 
 
