b209cfa549b695512dffa0b7e92cb2b221ceec77
lvalerom <lvaleromartin@gmail.com>
2013-01-16 14:10:05 +0100

    Faltaria probar que tire y mirar el caso de que tenga distinto numero de cores y columnas, pero en principio si es igual deberia tirar...

--- practica2/hackerMPI.c
+++ practica2/hackerMPI.c
@@ -5,9 +5,14 @@
 #define TAG 1
 #define DEBUG 0
 
+#define MAXCOLS 3
+#define MAXROWS 3
+#define NUMCORES 3
+
 
 int main(int argc, char* argv[]) {
 
+	/*
 	int size, rank;
 	int data[3];
 	int ok[3] = {1,1,1};
@@ -15,12 +20,59 @@ int main(int argc, char* argv[]) {
 					{0,0,0},
 					{0,0,0}};
 
+	*/
+
+	int size, rank;
+	int Matrix[MAXROWS][MAXCOLS];
+	int bitMap[MAXCOLS];
+
+	int i,j;
+	for(i = 0; i < MAXROWS; i++)
+		for(j = 0; j < MAXCOLS; j++)
+			Matrix[i][j] = 0;
+
+	for(i = 0; i < MAXCOLS; i++)
+		bitMap[i] = 1;
+
 	// Init
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+	if(rank == 0)
+	{
+		for(i = 1;i < NUMCORES-1; i++)
+			MPI_send();
+	}
 
+	initSystem(rank);
+
+	int fin = 0;
+	do
+	{
+		compareMatrixRow(Matrix[rank],bitMap,rank);
+		int auxFin = 0;
+		for(int i = 1;i < MAXCOLS; i++)
+		{
+			if(auxFin == 0) auxFin = bitMap[i];
+			Matrix[rank] = Matrix[rank][i] + bitMap[i];
+		}
+
+		if(auxFin == 0) fin = 1;
+
+	}while(fin == 0);
+
+	if(rank != 0)
+		MPI_send();
+	else
+	{
+		for(i = 1; i < MAXCOLS; i++)
+			MPI_receive();
+
+		//comparar la matriz final
+	}
+
+	/*
 
 	MPI_Scatter(res,9,MPI_INT,data,3,MPI_INT,0,MPI_COMM_WORLD);
 
@@ -49,7 +101,7 @@ int main(int argc, char* argv[]) {
 		printf("FAIL");
 	}
 
-
+	*/
 	MPI_Finalize();
 	exit(0);
 }
